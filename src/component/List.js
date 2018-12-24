9762cda6653faee68945be338b14e8ab0e40584e
perjerz3434 <perjerz3434@gmail.com>
2018-12-24 15:56:36 +0700

    add topNav

--- src/component/List.js
+++ src/component/List.js
@@ -1,15 +1,55 @@
 import React, { Component } from 'react';
 import Passport from './Passport';
+import { TextField } from '@material-ui/core';
+import location from '../assets/location.svg'
+import Topnav from './Topnav';
 
 class List extends Component {
+  state = {
+    location: '',
+    destination: ''
+  };
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+  
   render() {
     const bloggers = [
-      {id: 1, img: 'https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg', name: 'Michael', title: 'Easy to go to Chidlom', start: 'Asoke', end: 'Chidlom'}
+      {id: 1, img: 'https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg', name: 'Michael', title: 'Easy to go to Chidlom', start: 'Asoke', end: 'Chidlom', stars: 5}
     ];
     return (
       <div>
-        {bloggers.map(b => (<Passport img={b.img} name={b.name} title={b.title} start={b.start} end={b.end} handleClick={e => this.selectPassport(b.id)}></Passport>))}
-      </div>  
+        <Topnav/>
+        <form noValidate autoComplete="off" style={{marginLeft: 20, marginRight: 20, marginBottom: 20 }}>
+          <div className="d-flex align-items-center" style={{marginBottom: 20}}>
+            <img className="align-self-end" style={{marginBlockEnd: 5}} src={location} />
+            <TextField
+              style={{marginLeft: 20}}
+              id="standard-name"
+              label="Your Location"
+              value={this.state.location}
+              onChange={this.handleChange('location')}
+              margin="auto"
+              width="100%"
+            />
+          </div>
+          <div className="d-flex align-items-center" style={{marginBottom: 20}}>
+            <TextField
+              style={{marginLeft: 40}}
+              id="standard-name"
+              label="Destination"
+              value={this.state.destination}
+              onChange={this.handleChange('destination')}
+              margin="auto"
+              width="100%"
+            />
+          </div>
+          {bloggers.map(b => (<Passport img={b.img} name={b.name} title={b.title} start={b.start} end={b.end} handleClick={e => this.selectPassport(b.id)} stars={b.stars}></Passport>))}
+          </form>
+        </div>
     );
   }
   selectPassport(id) {
