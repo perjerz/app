b56da35f68c2a59981cccd2d2634a29a40ff2ec9
perjerz3434 <perjerz3434@gmail.com>
2018-12-24 12:36:18 +0700

    add list.js

--- src/component/List.js
+++ src/component/List.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import bus from '../assets/bus.svg';
+import motorcycle from '../assets/motorcycle.svg';
+import train from '../assets/train.svg';
+
+const detail = {
+  width: '50%'
+};
+
+const icon = {
+  width: '50px',
+  height: '50px'
+}
+
+const border = {
+  border: '1px solid #000',
+}
+
+const column = {borderLeft: '1px solid #000', padding: 10};
 
 class List extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <p>
-            LIST!!!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div style={border} className="d-flex flex-row">
+          <div className="d-flex align-items-center">
+            <img style={icon} alt="bus" src={bus}/>
+          </div>
+          <div style={{...column, flex: 0.2}} className="d-flex">
+            <p>1. Go to Victory Monoment</p>
+          </div>
+          <div style={{...column, flex: 0.3}} className="d-flex flex-column">
+            <div style={{whiteSpace: 'pre'}}>20 Minutes</div>
+          </div>
+        </div>
+        <div style={border} className="d-flex flex-row">
+          <div className="d-flex align-items-center">
+            <img style={icon} alt="train" src={train}/>
+          </div>
+          <div style={{...column, flex: 0.3}} className="d-flex">
+            <p>2. Go to BTS Mochit</p>
+          </div>
+          <div style={{...column, flex: 0.3}} className="d-flex flex-column">
+            <div style={{whiteSpace: 'pre'}}>20 Minutes</div>
+          </div>
+        </div>
       </div>
     );
   }
