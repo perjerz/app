61ca8252460d41b9e04130aa3b2f5ed6c22c7f38
perjerz3434 <perjerz3434@gmail.com>
2018-12-24 14:47:08 +0700

    fix space

--- src/component/List.js
+++ src/component/List.js
@@ -21,8 +21,6 @@ const border = {
 const column = {borderLeft: '1px solid #000', padding: 10};
 
 class List extends Component {
-
-
   render() {
     const bloggers = [
       {id: 1, img: 'https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg', name: 'Michael', title: 'Easy to go to Chidlom', start: 'Asoke', end: 'Chidlom'}
