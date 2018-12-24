e9bf64d9b41fe2c75dbbc52c947adc99747dd89a
perjerz3434 <perjerz3434@gmail.com>
2018-12-24 10:23:57 +0700

    add route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Router } from 'react-router-dom'
+
+const AppWithRouter = () => (
+    <Router>
+        <App />
+    </Router>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
